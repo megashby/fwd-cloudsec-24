415b838ac9e9a8fbdb57ae5e950dee8082d41532
Meg <meg@hgs-MacBook-Pro.lan>
2024-05-18 18:01:15 -0400

    move to remote state

--- vpc-new-jersey/main.tf
+++ vpc-new-jersey/main.tf
@@ -2,6 +2,23 @@ provider "aws" {
   region = "us-west-2"
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.30.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "fwd-cloudsec-remote-state"
+    key            = "vpc-new-jersey/terraform.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+    dynamodb_table = "fwd-cloudsec-locks"
+  }
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = ">=5.0.0"
