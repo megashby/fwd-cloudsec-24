ab51fe2b80bdea68830a959532a2fb9e4a99fc7f
Meg <meg@hgs-MacBook-Pro.lan>
2024-05-25 16:33:47 -0400

    add tags for private subnets in vpcs

--- vpc-new-jersey/main.tf
+++ vpc-new-jersey/main.tf
@@ -32,4 +32,8 @@ module "vpc" {
   private_subnets = ["10.1.1.0/24", "10.1.2.0/24"]
   public_subnets  = ["10.1.3.0/24", "10.1.4.0/24"]
 
+  private_subnet_tags = {
+    "Tier": "Private"
+  }
+
 }
--- vpc-new-york/main.tf
+++ vpc-new-york/main.tf
@@ -32,4 +32,8 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
 
+  private_subnet_tags = {
+    "Tier": "Private"
+  }
+
 }
