e12558dc69f92ae1d4fa160544ac93b2612906c1
Meg Ashby <megashby@comcast.net>
2024-05-27 19:05:03 -0400

    fix typo with zones

--- vpc-new-jersey/sg.tf
+++ vpc-new-jersey/sg.tf
@@ -9,3 +9,13 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https_vpc" {
+  # for_each = toset(module.vpc.private_subnets_cidr_blocks)
+
+  security_group_id = aws_security_group.ec2_sg.id
+  cidr_ipv4         = module.vpc.vpc_cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
