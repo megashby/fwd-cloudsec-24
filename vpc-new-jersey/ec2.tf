adbdc810e0217ce8220f382c4aa9968b09274e42
Meg Ashby <megashby@comcast.net>
2024-05-27 09:29:57 -0400

    subscribe nj and conneticut to additional vpce for ssm

--- vpc-new-jersey/ec2.tf
+++ vpc-new-jersey/ec2.tf
@@ -9,11 +9,11 @@ data "aws_ami" "amzn-linux-2023-ami" {
 }
 
 resource "aws_instance" "new_jersey_ec2" {
-  ami                      = data.aws_ami.amzn-linux-2023-ami.id
-  iam_instance_profile = aws_iam_instance_profile.iam_profile.name
-  instance_type            = "t2.micro"
-  subnet_id                = module.vpc.private_subnets[0]
-  vpc_security_group_ids   = [resource.aws_security_group.ec2_sg.id]
+  ami                    = data.aws_ami.amzn-linux-2023-ami.id
+  iam_instance_profile   = aws_iam_instance_profile.iam_profile.name
+  instance_type          = "t2.micro"
+  subnet_id              = module.vpc.private_subnets[0]
+  vpc_security_group_ids = [resource.aws_security_group.ec2_sg.id]
 
   tags = {
     Name = "new-jersey-ec2"
