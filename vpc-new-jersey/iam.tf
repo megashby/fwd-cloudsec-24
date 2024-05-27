d664c4f9e98d30e68835f98770ad54ddfb5ba29f
Meg <meg@hgs-MacBook-Pro.lan>
2024-05-27 09:11:15 -0400

    add additional ec2 permisisons

---
+++ vpc-new-jersey/iam.tf
@@ -0,0 +1,30 @@
+data "aws_iam_policy_document" "ec2_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ec2" {
+  name               = "instance_role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "sqs_policy" {
+  role       = aws_iam_role.ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSReadOnlyAccess"
+}
+
+resource "aws_iam_instance_profile" "iam_profile" {
+  name = "ec2-profile"
+  role = aws_iam_role.ec2.name
+}   
