feb147af16bb935a39abd417663b27f967602b19
Meg Ashby <megashby@comcast.net>
2024-05-31 14:54:29 -0400

    add VPCE policy for denying access from new jersey

--- vpc-new-york/data.tf
+++ vpc-new-york/data.tf
@@ -4,3 +4,11 @@ data "aws_vpc" "vpc" {
   count = length(data.aws_vpcs.all_vpcs.ids)
   id    = tolist(data.aws_vpcs.all_vpcs.ids)[count.index]
 }
+
+data "aws_vpc" "vpc_new_jersey" {
+  tags = { "Name" : "vpc-new-jersey" }
+}
+
+data "aws_vpc" "vpc_pennsylvania" {
+  tags = { "Name" : "vpc-pennsylvania" }
+}
